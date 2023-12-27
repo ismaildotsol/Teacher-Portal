e695fdfb827474efe7675e2671222b0c27917027
ismaildotsol <dotbdsol@gmail.com>
2023-12-27 11:42:07 +0600

    icon added

--- src/app/dashboard/component/menu.js
+++ src/app/dashboard/component/menu.js
@@ -3,50 +3,53 @@ import { TbLayoutDashboard } from "react-icons/tb";
 import { IoBookmarksOutline } from "react-icons/io5";
 import { TiMessageTyping } from "react-icons/ti";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
-
+import { MdOutlinePersonalInjury } from "react-icons/md";
+import { MdSocialDistance } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
 
 
 const menu = () => {
     return (
-        <div className= 'menu'>
-        <h1 className='m-8'>
-        <strong>Menu</strong>
-        </h1>
+        <div className='menu'>
+            <h1 className='m-8'>
+                <strong>Menu</strong>
+            </h1>
             <div className="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
-            <TbLayoutDashboard />
+                <TbLayoutDashboard />
                 <span className="text-[15px] ml-4 text-gray-200">Dashboard</span>
             </div>
 
             <div className="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
-            <IoBookmarksOutline />
+                <IoBookmarksOutline />
                 <span className="text-[15px] ml-4 text-gray-200">Bookmark</span>
             </div>
 
             <hr className="my-4 text-gray-600" />
             <div className="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
                 <TiMessageTyping />
-                    <span className="text-[15px] ml-4 text-gray-200">Messages</span>
+                <span className="text-[15px] ml-4 text-gray-200">Messages</span>
             </div>
 
             <div className="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
-            <IoChatboxEllipsesOutline />
-                    <span className="text-[15px] ml-4 text-gray-200">ChatBox</span>
+                <IoChatboxEllipsesOutline />
+                <span className="text-[15px] ml-4 text-gray-200">ChatBox</span>
             </div>
 
-            <div
-                className=" leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto"
-                id="submenu"
-            >
-                <h1 className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">
+            <div className="leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto" id="submenu">
+                <div className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1 flex items-center">
+                    <MdSocialDistance  className="mr-2" />
                     Social
-                </h1>
-                <h1 className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">
+                </div>
+                <div className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1 flex items-center">
+                    <MdOutlinePersonalInjury className="mr-2" />
                     Personal
-                </h1>
-                <h1 className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">
+                </div>
+                <div className="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1 flex items-center">
+                    <FaUserFriends className="mr-2" />
                     Friends
-                </h1>
+                </div>
             </div>
+
             <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
                 <i className="bi bi-box-arrow-in-right"></i>
                 <span className="text-[15px] ml-4 text-gray-200">Logout</span>
