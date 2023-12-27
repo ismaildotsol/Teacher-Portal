dd8fd3b8ecc22e1659242b53d1d806c1b52ed4e4
ismaildotsol <dotbdsol@gmail.com>
2023-12-27 10:41:44 +0600

    shifted components call in root page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-export default function Home() {
+import Navbar from "./dashboard/component/navBar";
+import Sidebar from "./dashboard/component/sideBar";
+import Card from "./dashboard/component/card";
+const Home = () => {
   return (
-    <main>
-      <h1>Hellow</h1>
+    <main className="flex ">
+      <div>
+        <Sidebar />
+      </div>
+
+      <div className="flex-1">
+        <Navbar />
+           <Card/>
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
