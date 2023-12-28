e961097f90f562d790c9a87a7938f1c2942267ba
ismaildotsol <dotbdsol@gmail.com>
2023-12-28 14:56:47 +0600

    side will auto hide for device screen retio

--- src/app/dashboard/component/sideBar.js
+++ src/app/dashboard/component/sideBar.js
@@ -1,13 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaAlignRight } from "react-icons/fa6";
 import Menu from "./menu";
 
 const SideBar = () => {
   const [open, setOpen] = useState(true);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setOpen(window.innerWidth > 768);
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className={`bg-gray-800 text-white h-screen scro overflow-y-auto ${open ? "w-72" : "w-20"} duration-300 relative`}>
+    <div className={`bg-gray-800 text-white h-screen overflow-y-auto ${open ? "w-72" : "md:w-20 hidden md:block"} duration-300 relative`}>
       
       <FaAlignRight
         className={`text-2xl absolute top-8 transform -translate-y-1/2 ${
